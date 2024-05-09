29275618787e7ab0750001b1ad4394def6712753
Siddhi <100768681+siddhisalunkhe05@users.noreply.github.com>
2024-05-09 08:47:20 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+    return a + b
+
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Call the add_numbers function with user input
+result = add_numbers(num1, num2)
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", result)
+
